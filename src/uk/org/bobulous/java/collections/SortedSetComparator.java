8c9bb3911738ba6d196e20468c01d27fcbb851f0
Bobulous <Bobulous@users.noreply.github.com>
2015-04-27 21:19:52 +0100

    Added Mozilla Public License 2.0 notice to top of each class.

--- src/uk/org/bobulous/java/collections/SortedSetComparator.java
+++ src/uk/org/bobulous/java/collections/SortedSetComparator.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2015 Bobulous <http://www.bobulous.org.uk/>.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package uk.org.bobulous.java.collections;
 
 import java.util.Comparator;
